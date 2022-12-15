8887fe09c38d35a03e9880cea9490ac4a3df4368
Donghan Kim <donghankim14@gmail.com>
2022-12-15 01:56:44 -0500

    still need to finish graph .txt format

--- graphGenerator.py
+++ graphGenerator.py
@@ -1,43 +1,49 @@
 import osmnx as ox
 ox.settings.log_console = False
+ox.settings.use_cache = True
 
 import numpy as np
 import networkx as nx
 import pandas as ps
 import geopandas as gpd
 import matplotlib.pyplot as plt
-import os, pdb
+import os, math, argparse, pdb
+
+# for sanity check
+def haversine(coord1, coord2):
+    lon1, lat1 = coord1
+    lon2, lat2 = coord2
+    R = 6371000  # radius of Earth in meters
+    phi_1 = math.radians(lat1)
+    phi_2 = math.radians(lat2)
+    
+    delta_phi=math.radians(lat2-lat1)
+    delta_lambda=math.radians(lon2-lon1)
+
+    a=math.sin(delta_phi/2.0)**2+\
+            math.cos(phi_1)*math.cos(phi_2)*\
+            math.sin(delta_lambda/2.0)**2
+    c=2*math.atan2(math.sqrt(a),math.sqrt(1-a))
+
+    meters = R*c
+    print(meters)
 
 
-def geocode_address(address, crs=4326):
-    geocode = gpd.tools.geocode(address, provider='nominatim', user_agent="drive time demo").to_crs(crs)
-    return (geocode.iloc[0].geometry.y, geocode.iloc[0].geometry.x)
-
-def create_new_graph(loc, dist):
-    # not very accurate
-    # location_point = geocode_address(address)
-    # G = ox.graph_from_point(location_point, dist=r, dist_type="bbox", network_type="walk")
-    # G = ox.graph_from_point(loc, dist=dist, dist_type="network", network_type="walk")
-
-    G = ox.graph_from_place('Columbia University', network_type='walk')
-    G = ox.add_edge_speeds(G, hwy_speeds = 5.1)
-    G = ox.add_edge_travel_times(G)
-    #ox.save_graphml(G, filepath="./GraphML/manhattan.graphml", gephi=True)
-    #fig, ax = ox.plot_graph(G, show=False, save=True, close=True, node_color="r", edge_color = 'g', filepath="./GraphML/manhattan.png")
-
+def createTxt(G, TXT_PATH):
+    fp = open(TXT_PATH, "w")
     size = G.number_of_nodes()
     gdf_nodes, gdf_edges = ox.graph_to_gdfs(G)
     adj_list = []
 
-    # work on this
-    idx = 0
+    idx = 0    
     for u, outNode in gdf_edges.groupby(level=0):
         ux = G.nodes[u]['x']
         uy = G.nodes[u]['y']
         uName = str(uy) + "&" + str(ux)
-        data = str(idx) + ":" + uName + "***"
+        data = str(idx) + ":" + uName + "*"
         tmp = ""
         for v in outNode.iterrows():
+            pdb.set_trace()
             vx = G.nodes[v[0][1]]['x']
             vy = G.nodes[v[0][1]]['y']
             vName = str(vy) + "&" + str(vx)
@@ -46,39 +52,65 @@ def create_new_graph(loc, dist):
         adj_list.append(data+tmp)
         idx += 1
 
-    with open("columbia.txt", "w") as fp:
-        for line in adj_list:
-            fp.write(line + "\n")
+    for line in adj_list:
+        fp.write(line + "\n")
 
-    print("graph saved!")
+    fp.close()
+    print("graph .txt saved!")
 
-def load_graph(filepath):
-    G = ox.load_graphml(filepath)
-    return G
+def saveGraph(G, place):
+    DIR_PATH = os.path.join('./graphData/', place.replace(" ", "").strip())
+    ML_PATH = os.path.join(DIR_PATH, place.replace(" ", "").strip() + ".graphml")
+    PNG_PATH = os.path.join(DIR_PATH, place.replace(" ", "").strip() + ".png")
+    TXT_PATH = os.path.join(DIR_PATH, place.replace(" ", "").strip() + ".txt")
 
-def main():
-    # not very accruate
-    # addr = "Columbia University, New York, 10027"
-    # location_point = geocode_address(addr)
-    # G = ox.graph_from_point(loc, dist=350, dist_type="network", network_type="walk")
-    # fig, ax = ox.plot_graph(G, node_color="r", edge_color = 'g')
-    # G = ox.graph_from_place('Columbia University', network_type='walk')
-    # G = load_graph("./GraphML/columbia.graphml")
+    if not os.path.isdir(DIR_PATH):
+        os.mkdir(DIR_PATH)
+    else:
+        print(place + " graph data already exists...")
+        return
+    
+    print("creating graph data for " + place + "...") 
+    ox.save_graphml(G, filepath = ML_PATH, gephi = True)
+    print(".graphml saved!")
+    fig, ax = ox.plot_graph(G, show = False, save = True, close = True, node_color = "r", edge_color = 'g', filepath = PNG_PATH)
+    print(".png saved!")
+    # createTxt(G, TXT_PATH)
+       
 
-    create_new_graph(0, 0)
-    #G = ox.graph_from_place('Columbia University', network_type='walk')
-    # ox.save_graphml(G, filepath="./GraphML/manhattan.graphml", gephi=True)
-    # G = load_graph("./GraphML/manhattan.graphml")
-    #fig, ax = ox.plot_graph(G, show=True, save=False, close=False, node_color="r", node_size = 2.5, edge_color = 'g', filepath="./GraphML/manhattan.png")
+
+
+def create_new_graph(place):
+    G = ox.graph_from_place(place, network_type = "all", simplify = True)
+    G = ox.add_edge_speeds(G, hwy_speeds = 5.1) # for generating desired edges
+    G = ox.add_edge_travel_times(G)
+    saveGraph(G, place)
+    
+        
+
+def load_graph(place):
+    DIR_PATH = os.path.join('./graphData/', place.replace(" ", "").strip())
+    ML_PATH = os.path.join(DIR_PATH, place.replace(" ", "").strip() + ".graphml")
+    G = ox.load_graphml(ML_PATH)
+    
+
+    
     pdb.set_trace()
-
-
-    # columbia (211, 562)
-    # manhattan (42366, 145774)
-
-
 
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--generate", type = str, help = "enter the name of the place you want to download")
+    parser.add_argument("--load",  type = str, help = "load an existing graph (folder name)")
+    args = parser.parse_args()
+
+    if args.generate:
+        create_new_graph(args.generate)
+    elif args.generate:
+        load_graph(args.load)
+    else:
+        print("*** You have to either generate or load a graph ***")
+        
+
+
